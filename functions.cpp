a9f92c2979772776cb59739f850ead5d16a344c7
rincemust <aadi.naik@gmail.com>
2017-03-27 16:16:54 +0530

    Update functions.cpp

--- functions.cpp
+++ functions.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 #include <fstream>
 #include <string.h>
+#include "player.h"
 
 using namespace std;
 
 
 
-int action()
+int action(Player X)                                                //Function that returns an integer to perform the corresponding action
 {
     int line = 1;
     char input[101];
@@ -16,15 +17,19 @@ int action()
             cout << "This is too long to take in. Your input must be within 100 characters." << endl;
     }while(strlen(input) >= 101);
 
-    char verb[8];
+    char verb[8];                                                   //Maximum length of verb in dictionary is 8 chars
     for(int i = 0; input[i] != ' '; i++){
         verb[i] = input[i];
     }
     verb[7] = '\0';
 
+    if(!(strcmp(verb, "inv"))){                                     //inv is verb to display inventory
+        X.show_inv();
+    }
+
     ifstream iFile;
     iFile.open("data\\dictionary\\verbs.dat");
-    while(iFile){
+    while(iFile){                                                   //returns line number of verb entered, or 0 if verb doesn't exist
         string x;
         getline(iFile, x);
         if(x == verb){
