409f8374b6b3d2a8061156834a61603b1b304617
rincemust <aadi.naik@gmail.com>
2017-04-06 23:15:22 +0530

    Updated Room.h and Room.cpp

--- Room.h
+++ Room.h
@@ -4,9 +4,21 @@
 class Room
 {
   std::string name;
+  int code, room_f, room_l, room_r, room_b;
 
  public:
-  
+  Room(std::string x, int a){
+    std::string path = "data/rooms/";
+    name = path + x;
+    code = a;
+    room_f = -1;
+    room_l = -1;
+    room_r = -1;
+    room_b = -1;
+  }
+
+  void display();
+  bool chkroom(int);
 };
 
 #endif
