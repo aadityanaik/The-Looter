3dc027c0d1a0c7584e8a0b5ddd7a78ad7ed37c3d
rincemust <aadi.naik@gmail.com>
2017-04-11 23:51:44 +0530

    updated parts of the story, defined a few of the room functions, made a class for inventory

---
+++ Inventory.h
@@ -0,0 +1,9 @@
+#ifndef _INVENTORY_H
+#define _INVENTORY_H
+
+#include "Items.h"
+
+class Inventory
+{
+  
+};
---
+++ Room.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+
+//custom headers
+#include "Room.h"
+#include "File.h"
+
+void Room::display()
+{
+  File R(name);
+  R.disp();
+}
+
+bool Room::chkroom(int number)
+{
+  if(number == code){
+    return true;
+  }
+
+  else{
+    return false;
+  }
+}
+
+void Room::set_para(int front, int left, int right, int back)
+{
+  room_f = front;
+  room_l = left;
+  room_r = right;
+  room_b = back;
+}
+
+int Room::return_code(char x = 'i')
+{
+  switch(x){
+  case 'f':
+    return room_f;
+
+  case 'l':
+    return room_l;
+
+  case 'r':
+    return room_r;
+
+  case 'b':
+    return room_b;
+
+  default:
+    return code;
+
+  }
+}
